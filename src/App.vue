51c05afd9419ef062f85531b6eaed99b10da1d81
Ian Cameron <ian.kameron@gmail.com>
2019-05-21 18:24:24 +0900

    Basic vue app setup w toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Take a week off with friends!</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-form>
+        <v-layout row wrap>
+          <v-text-field
+            label="Where to?"
+            placeholder="Dreamland"
+            v-model="destination"
+          ></v-text-field>
+          <v-menu
+            v-model="menu"
+            :close-on-content-click="true"
+            :nudge-right="40"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="date"
+                prepend-icon="event"
+                label="When do you leave?"
+                readonly
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+          </v-menu>
+          <v-btn @click="getTripData">Go!!</v-btn>
+        </v-layout>
+      </v-form>
+
+      <v-flex>
+        
+      </v-flex>
+    </v-toolbar>
+
+    <v-content>
+      <StartScreen/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StartScreen from './components/StartScreen'
+import Axios from 'axios'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    StartScreen
+  },
+  data () {
+    return {
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
+      destination: ""
+    }
+  },
+  methods: {
+    getTripData: async function ()  {
+      console.log(this.date, this.destination);
+      const response = await Axios({
+        method: 'post',
+        url: '/api/travel',
+        data: {
+          date: this.date,
+          destination: this.destination
+        }
+      });
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
 
+Vue.use(Vuex);
+Vue.use(Vuetify);
 Vue.config.productionTip = false
 
 new Vue({
