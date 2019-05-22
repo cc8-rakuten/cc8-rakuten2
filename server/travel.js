45d66be65d29793cc2b5934c42b05629ea8ab0cb
Konstantin Schlegel and Yasuhito Miyazaki <konst2011+kizayami.yasu@hotmail.de>
2019-05-22 14:08:43 +0900

    added api to get ip address

--- server/travel.js
+++ server/travel.js
@@ -6,27 +6,41 @@ const axios = require("axios");
 const router = express.Router();
 
 router.post("/travel", async (req, res) => {
-  console.log(req.headers);
-  res.json(req.headers["x-forwarded-for"]);
+  let sourceIP = req.headers["x-forwarded-for"];
   const date = req.body.date;
   const destination = req.body.destination;
   const radius = "150";
-  const lng = req.body.source.lng;
-  const lat = req.body.source.lat;
-  const response = await axios({
+  if (sourceIP.indexOf("127.") > -1) {
+    sourceIP = "121.1.253.218";
+  }
+  await axios({
     method: "GET",
-    url: `https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius?radius=${radius}&lng=${lng}&lat=${lat}`,
+    url: `https://moocher-io-ip-geolocation-v1.p.rapidapi.com/${sourceIP}`,
     headers: {
-      "X-RapidAPI-Host": "cometari-airportsfinder-v1.p.rapidapi.com",
+      "X-RapidAPI-Host": "moocher-io-ip-geolocation-v1.p.rapidapi.com",
       "X-RapidAPI-Key": process.env.AIRPORT_API_KEY
     }
   })
+    .then((result) => {
+      console.log(result.data);
+      const lng = result.data.ip.longitude;
+      const lat = result.data.ip.latitude;
+      // res.json(mainAirports);
+      return axios({
+        method: "GET",
+        url: `https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius?radius=${radius}&lng=${lng}&lat=${lat}`,
+        headers: {
+          "X-RapidAPI-Host": "cometari-airportsfinder-v1.p.rapidapi.com",
+          "X-RapidAPI-Key": process.env.AIRPORT_API_KEY
+        }
+      });
+    })
     .then((result) => {
       const allAirports = result.data;
       const mainAirports = allAirports
         .filter((airport) => airport.themes.length > 0)
         .map(({ code, city, name }) => ({ code, city, name }));
-      // res.json(mainAirports);
+      res.json(mainAirports);
     })
     .catch(() => res.status(400));
 });
