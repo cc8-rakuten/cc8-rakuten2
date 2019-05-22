d4113de9992d824656d70a76f30a0e23d4934498
Konstantin Schlegel and Yasuhito Miyazaki <konst2011+kizayami.yasu@hotmail.de>
2019-05-22 11:48:14 +0900

    change unirest to axios

--- server/travel.js
+++ server/travel.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const unirest = require("unirest");
+const axios = require("axios");
 // require("dotenv").config();
 
 const router = express.Router();
 
 router.post("/travel", async (req, res) => {
-  let allAirports;
-  unirest.get("https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius?radius=150&lng=139.727880&lat=35.658187")
-    .header("X-RapidAPI-Host", "cometari-airportsfinder-v1.p.rapidapi.com")
-    .header("X-RapidAPI-Key", process.env.AIRPORT_API_KEY)
-    .end(result => {
-      const allAirports = result.body;
+  const date = req.body.date;
+  const destination = req.body.destination;
+  const radius = "150";
+  const lng = "139.727880";
+  const lat = "35.658187";
+  const response = await axios({
+    method: "GET",
+    url: `https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius?radius=${radius}&lng=${lng}&lat=${lat}`,
+    headers: {
+      "X-RapidAPI-Host": "cometari-airportsfinder-v1.p.rapidapi.com",
+      "X-RapidAPI-Key": process.env.AIRPORT_API_KEY
+    }
+  })
+    .then((result) => {
+      const allAirports = result.data;
       const mainAirports = allAirports
-        .filter(airport => airport.themes.length > 0)
-        .map(({code, city, name}) => ({code, city, name}));
+        .filter((airport) => airport.themes.length > 0)
+        .map(({ code, city, name }) => ({ code, city, name }));
       res.json(mainAirports);
-    });
-
+    })
+    .catch(() => res.status(400));
 });
 
 module.exports = router;
