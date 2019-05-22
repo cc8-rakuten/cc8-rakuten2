99a5e34dcedfe46f5837b0e307f02dedd1e05c99
Konstantin Schlegel and Yasuhito Miyazaki <konst2011+kizayami.yasu@hotmail.de>
2019-05-22 18:09:19 +0900

    mvp api to get flights done

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import Vuex from 'vuex'
-import Vuetify from 'vuetify'
-import flights from "../data/flights.json"
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import Vuex from "vuex";
+import Vuetify from "vuetify";
+import flights from "../data/flights.json";
 
-import VuetifyGoogleAutocomplete from 'vuetify-google-autocomplete';
+import VuetifyGoogleAutocomplete from "vuetify-google-autocomplete";
+import { from } from "rxjs";
 
 Vue.use(VuetifyGoogleAutocomplete, {
-  apiKey: 'AIzaSyAn19RJD6FC5aLwzr0AbsmzP4rmQjoJENU',
+  apiKey: "AIzaSyAn19RJD6FC5aLwzr0AbsmzP4rmQjoJENU",
   language: "en"
 });
 
 Vue.use(Vuex);
 Vue.use(Vuetify);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 const store = new Vuex.Store({
   state: {
@@ -24,14 +25,14 @@ const store = new Vuex.Store({
   },
   mutations: {
     setTravelIdea(state, newTravelIdea) {
-      console.log(state.flightsInfo)
+      console.log(state.flightsInfo);
       state.travelIdea = newTravelIdea;
       state.submittedStatus = true;
     }
   }
-})
+});
 
 new Vue({
   store,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount("#app");
