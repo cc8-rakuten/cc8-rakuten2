7eb0d05cd3962921b6dcaef2e589422cca1997d0
Ian Cameron and Konstantin Schlegel <ian.kameron+konst2011@gmail.com>
2019-05-23 16:46:24 +0900

    finished MVP backend

--- server/travel.js
+++ server/travel.js
@@ -16,22 +16,44 @@ router.post("/travel", async (req, res) => {
   let sourceIP = req.headers["x-forwarded-for"];
   console.log("entering");
   const radius = "150";
-  let fromCity, toCity;
+  let fromCity, toCity, pictureURL;
   toCity = req.body.destination.name + ", " + req.body.destination.country;
+  const imgSearch =
+    "sightseeing+" +
+    req.body.destination.name +
+    "+" +
+    req.body.destination.country;
   // console.log(req.body.destination);
   if (sourceIP.indexOf("127.") > -1) {
     sourceIP = "121.1.253.218";
   }
-  await axios({
-    method: "GET",
-    url: `https://moocher-io-ip-geolocation-v1.p.rapidapi.com/${sourceIP}`,
-    headers: {
-      "X-RapidAPI-Host": "moocher-io-ip-geolocation-v1.p.rapidapi.com",
-      "X-RapidAPI-Key": process.env.AIRPORT_API_KEY
-    }
-  })
-    .then((result) => {
+
+  const initialRequests = [
+    axios({
+      method: "GET",
+      url: `https://moocher-io-ip-geolocation-v1.p.rapidapi.com/${sourceIP}`,
+      headers: {
+        "X-RapidAPI-Host": "moocher-io-ip-geolocation-v1.p.rapidapi.com",
+        "X-RapidAPI-Key": process.env.AIRPORT_API_KEY
+      }
+    }),
+    axios({
+      method: "GET",
+      url: `https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?autoCorrect=false&pageNumber=1&pageSize=10&q=${imgSearch}&safeSearch=true`,
+      headers: {
+        "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+        "X-RapidAPI-Key": process.env.AIRPORT_API_KEY
+      }
+    })
+  ];
+
+  await Promise.all(initialRequests)
+    .then((results) => {
       console.log("entering processing of geoloc");
+      let imgResult = results[1];
+      pictureURL = imgResult.data.value[4].url;
+      console.log(pictureURL);
+      let result = results[0];
       const fromLng = result.data.ip.longitude;
       const fromLat = result.data.ip.latitude;
       // console.log(result.data);
@@ -67,7 +89,8 @@ router.post("/travel", async (req, res) => {
         from: fromCity,
         to: toCity,
         fromAirport: fromToAirports[0],
-        toAirport: fromToAirports[1]
+        toAirport: fromToAirports[1],
+        pictureURL
       };
       console.log(response);
       res.json(response);
@@ -109,8 +132,6 @@ router.post("/flights", async (req, res) => {
       console.log("entering processing of flight search sessions");
       return Promise.all(
         flights.map((flight) => {
-          console.log(Object.keys(flight));
-          console.log(flight.data);
           return axios({
             method: "GET",
             url: `https://apidojo-kayak-v1.p.rapidapi.com/flights/poll?searchid=${
@@ -138,12 +159,8 @@ router.post("/flights", async (req, res) => {
           { lowTotal: 9999999 }
         );
       });
-      const airportSummary1 = resultFlights[0].airportSummary;
-      const airportSummary2 = resultFlights[1].airportSummary;
       const price = resultFlights[0].lowTotal + resultFlights[1].lowTotal;
       res.json({
-        airportSummary1,
-        airportSummary2,
         price
       });
     })
